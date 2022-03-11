43d48245d54defe09fb1bda91ed9211c98cb7ae6
Stefan Brugger <stefpb@gmail.com>
2022-03-11 11:59:17 +0000

    sum->max

--- L0_regularization/l0_layers.py
+++ L0_regularization/l0_layers.py
@@ -81,7 +81,8 @@ class L0Activation(Module):
         logpw_col = - (.5 * self.prior_prec * self.activations.pow(2)) - self.lamba
         logpw = ((1 - self.cdf_qz(0)) * logpw_col).sum(1)
 
-        logpw = logpw.sum()
+        # logpw = logpw.sum()
+        logpw = logpw.max()
 
         logpb = 0 if not self.use_bias else - torch.sum(.5 * self.prior_prec * self.bias.pow(2))
         # print(logpw)  
